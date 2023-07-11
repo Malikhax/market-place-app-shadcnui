388161d1a6e167adcb5e5ef43a1ab6e6f19f5f52
unknown <malikhax3@gmail.com>
2023-07-11 19:30:58 +0500

    added error resolve

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest,NextResponse } from "next/server"
-export const GET =async()=>{
-    return
-}
+// import { NextRequest,NextResponse } from "next/server"
+// export const GET =async()=>{
+//     return
+// }
